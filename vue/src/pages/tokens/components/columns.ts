6332873bfc24c9d27a731a1c977023bdcc1cd743
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-07-24 00:04:02 +0200

    vue tokens

--- vue/src/pages/tokens/components/columns.ts
+++ vue/src/pages/tokens/components/columns.ts
@@ -1,12 +1,15 @@
 import { ColumnDef } from '@tanstack/vue-table';
 import { Token } from '@/types/token';
+import { h } from 'vue';
+import DateTime from '@/components/date-time.vue';
+import Actions from '@/pages/tokens/components/actions.vue';
 
 export const columns: ColumnDef<Token>[] = [
   {
     accessorKey: 'name',
     header: 'Name',
     enableColumnFilter: true,
-    enableSorting: true
+    enableSorting: true,
   },
   {
     accessorKey: 'abilities',
@@ -19,10 +22,14 @@ export const columns: ColumnDef<Token>[] = [
     header: 'Created at',
     enableColumnFilter: true,
     enableSorting: true,
+    cell: ({ row }) => {
+      return h(DateTime, { date: row.original.created_at });
+    },
   },
   {
     id: 'actions',
     enableColumnFilter: false,
     enableSorting: false,
+    cell: ({ row }) => h('div', { class: 'flex items-center justify-end' }, [h(Actions, { token: row.original })]),
   },
 ];
--- vue/src/pages/tokens/index.vue
+++ vue/src/pages/tokens/index.vue
@@ -7,6 +7,9 @@ import Container from '@/components/container.vue';
 import Heading from '@/components/heading.vue';
 import DataTable from '@/components/data-table.vue';
 import { columns } from '@/pages/tokens/components/columns';
+import CreateToken from '@/pages/tokens/components/create-token.vue';
+import { Button } from '@/components/ui/button';
+import { PlusIcon } from 'lucide-vue-next';
 
 const breadcrumbs: BreadcrumbItem[] = [
   {
@@ -31,12 +34,12 @@ const page = usePage<{
       <div class="flex items-start justify-between">
         <Heading title="API Tokens" description="Here you can manage API tokens" />
         <div class="flex items-center gap-2">
-          <!--            <CreateToken>-->
-          <!--              <Button>-->
-          <!--                <PlusIcon />-->
-          <!--                Create token-->
-          <!--              </Button>-->
-          <!--            </CreateToken>-->
+          <CreateToken>
+            <Button>
+              <PlusIcon />
+              Create token
+            </Button>
+          </CreateToken>
         </div>
       </div>
       <DataTable :columns="columns" :paginatedData="page.props.tokens" />
